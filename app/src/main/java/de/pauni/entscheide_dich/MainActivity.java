415cddb1f63530eafa6deb7ef0db61a65f6cd514
Roni <roni-96@live.de>
2017-03-14 15:00:35 +0100

    QuestionManager reduziert

--- app/src/main/java/de/pauni/entscheide_dich/MainActivity.java
+++ app/src/main/java/de/pauni/entscheide_dich/MainActivity.java
@@ -1,13 +1,185 @@
 package de.pauni.entscheide_dich;
 
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+/*
+*   Die MainActivity verarbeitet die Eingaben des Nutzers und die Ausgabe der Frage
+*   über die UI und ist die Schnittstelle zwischen UI und dem questionManager. Über den
+*   questionManager holt sie sich die entspr. Einträge oder markiert Fragen als Favoriten.
+*
+*/
 public class MainActivity extends AppCompatActivity {
+    QuestionManager questionManager;
+    Handler handler;
+
+    boolean zufaellig           =   false;
+    //Declaring the views
+    TextView    tv_Fragen       =   null;
+    TextView    tv_Sendung      =   null;
+
+    ImageButton ib_naechste     =   null;
+    ImageButton ib_vorige       =   null;
+    ImageButton ib_share        =   null;
+    ImageButton ib_favorit      =   null;
+    ImageButton ib_zufaellig    =   null;
+    ImageButton ib_youtube      =   null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new SharedPrefs(this);
+        new DatabaseInitializer(this);
+
+        handler         =   new Handler();
+        questionManager =   new QuestionManager();
+        initViews();
+        regListeners();
+        frageAnzeigen(questionManager.getCurrent());
     }
+
+    private void frageAnzeigen(Question question) {
+        String  text    = question.getQuestionString();
+        String  guest   = question.getGuestString();
+        boolean favorit = question.isFavorit();
+
+        tv_Fragen.setText(text);
+        tv_Sendung.setText("Sendung mit "+guest);
+        if (favorit) {
+            ib_favorit.setColorFilter(getResources().getColor(R.color.nmr_background));
+            ib_favorit.setImageResource(R.drawable.ic_favorite_white_24dp);
+        } else {
+            ib_favorit.setColorFilter(getResources().getColor(R.color.icon_color));
+            ib_favorit.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+        }
+    }
+
+
+    private void initViews() {
+        tv_Fragen       =   (TextView)    findViewById(R.id.textview_Fragen);
+        tv_Sendung      =   (TextView)    findViewById(R.id.textview_sendung);
+
+        ib_naechste     =   (ImageButton) findViewById(R.id.imagebutton_naechste);
+        ib_share        =   (ImageButton) findViewById(R.id.imagebutton_share);
+        ib_vorige       =   (ImageButton) findViewById(R.id.imagebutton_zurueck);
+        ib_favorit      =   (ImageButton) findViewById(R.id.imagebutton_favorit);
+        ib_zufaellig    =   (ImageButton) findViewById(R.id.imagebutton_zufaellig);
+        ib_youtube      =   (ImageButton) findViewById(R.id.imagebutton_youtube);
+        ib_youtube      =   (ImageButton) findViewById(R.id.imagebutton_youtube);
+
+    }
+    private void regListeners() {
+        ib_naechste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (zufaellig) {
+                    frageAnzeigen(questionManager.getRandom());
+                } else {
+                    frageAnzeigen(questionManager.getNext());
+                }
+            }
+        });
+
+        ib_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String frage = tv_Fragen.getText().toString() + "\n\nEntscheide Dich! ist echt eine super App!";
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, frage);
+                startActivity(Intent.createChooser(i, "Teile die Frage mit Feinden:"));
+            }
+        });
+
+        ib_vorige.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frageAnzeigen(questionManager.getPrevious());
+            }
+        });
+
+
+        ib_favorit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getCurrentQuestion returns String[] = {"text", "sendung", "1" or "0"}
+                boolean favorite = questionManager.getCurrent().isFavorit();
+
+                if (!favorite) {
+                    questionManager.setFavorite(true);
+                    changeViewColor(ib_favorit, 200, R.color.icon_color, R.color.nmr_background);
+                    ib_favorit.setImageResource(R.drawable.ic_favorite_white_24dp);
+                } else {
+                    questionManager.setFavorite(false);
+                    changeViewColor(ib_favorit, 200, R.color.nmr_background, R.color.icon_color);
+                    ib_favorit.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+                }
+            }
+        });
+
+        ib_zufaellig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!zufaellig) {
+                    zufaellig = true;
+                    changeViewColor(ib_zufaellig, 500, R.color.icon_color, R.color.nmr_background);
+                } else {
+                    zufaellig = false;
+                    changeViewColor(ib_zufaellig, 500, R.color.nmr_background, R.color.icon_color);
+                }
+            }
+        });
+
+        ib_youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeViewColor(ib_youtube, 300, R.color.icon_color, R.color.nmr_background);
+                changeViewColor(ib_youtube, 300, R.color.nmr_background, R.color.icon_color);
+                startActivity(
+                        new Intent(Intent.ACTION_VIEW, Uri.parse(questionManager.getYTLink())));
+            }
+        });
+    }
+
+    private void changeViewColor(final ImageButton ib, int duration, int startColor, int endColor) {
+        // Load initial and final colors.
+        final int initialColor  =   getResources().getColor(startColor);
+        final int finalColor    =   getResources().getColor(endColor);
+
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Use animation position to blend colors.
+                float position = animation.getAnimatedFraction();
+                int blended = blendColors(initialColor, finalColor, position);
+
+                // Apply blended color to the view.
+                ib.setColorFilter(blended);
+            }
+        });
+
+        anim.setDuration(duration).start();
+    }
+    private int blendColors(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.rgb((int) r, (int) g, (int) b);
+    }
+
+
 }
--- app/src/main/java/de/pauni/entscheide_dich/SharedPrefs.java
+++ app/src/main/java/de/pauni/entscheide_dich/SharedPrefs.java
@@ -3,9 +3,7 @@ package de.pauni.entscheide_dich;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 
-import java.util.Arrays;
 
 /**
  * Diese SharedPreferences Helperclass kümmert sich um all den Code zum
@@ -22,9 +20,8 @@ class SharedPrefs extends Activity {
     private static SharedPreferences.Editor editor;
 
     //the keys of the things to save
-    private static final String LIST_NUMBER     = "ListNumber";
-    private static final String ENTRY_NUMBER    = "EntryNumber";
-    private static final String FAVORITES       = "Favorites";
+    private static final String INDEX_NUMBER = "question_index";
+    private static final String FIRST_START  = "first_start";
 
     SharedPrefs(Context c) {
         //Maybe I will need the context variable later...
@@ -32,96 +29,14 @@ class SharedPrefs extends Activity {
     }
 
 
-    //Setter and getter method
 
-    static void saveListNumber(int value) {
+
+    static void saveQuestionId(int value) {
         editor = prefs.edit();
-        editor.putInt(LIST_NUMBER, value);
+        editor.putInt(INDEX_NUMBER, value);
         editor.apply();
     }
-    static int getListNumber() {
-        return prefs.getInt(LIST_NUMBER, 1);
-    }
-
-
-    static void saveEntryNumber(int value) {
-        editor = prefs.edit();
-        editor.putInt(ENTRY_NUMBER, value);
-        editor.apply();
-    }
-    static int getEntryNumber() {
-        return prefs.getInt(ENTRY_NUMBER, 0);
-    }
-
-    static void addFavorite() {
-        String currentFavs  = prefs.getString(FAVORITES, "");
-        String newFav       = SharedPrefs.getListNumber()+"/"+SharedPrefs.getEntryNumber()+",";
-
-        editor = prefs.edit();
-        editor.putString(FAVORITES, currentFavs+newFav);
-        editor.apply();
-
-    }
-
-    static void removeFavorite() {
-        String favs = prefs.getString(FAVORITES, "");
-        String removeFav = SharedPrefs.getListNumber()+"/"+SharedPrefs.getEntryNumber()+",";
-        favs = favs.replace(removeFav, "");
-
-        editor = prefs.edit();
-        editor.putString(FAVORITES, favs);
-        editor.apply();
-    }
-
-
-    static int[][] getFavorites() {
-        //rawFavorites are formatted like this: "2/5,4/9,13/3" etc.
-        //1. number: listnum. | 2. number: entrynum. | slash: numdevider | comma: entrydevider
-        String rawFavorites = prefs.getString(FAVORITES, "");
-
-        if (rawFavorites.equals("")) {
-            return null;
-        }
-        Log.d("SharedPrefs:    ", rawFavorites);
-        //removing the redundant and potentially error causing "," at the end
-        //rawFavorites = rawFavorites.substring(0, rawFavorites.length()-1);
-
-        //rawListEntries[] are formatted like this: "2/5" "4/9" "13/3" etc.
-        String[] rawListEntries = rawFavorites.split(",");
-
-
-        //those two arrays will only contain listnumbers or entrynumbers
-        int arraySize           = rawListEntries.length;
-        int[] listNumbers    = new int[arraySize];
-        int[] entryNumbers   = new int[arraySize];
-
-        String[] tmpSplitter = {"1", "1"};
-        for (int i=0; i<arraySize; i++) {
-            //tmpSplitter will contain the listnummber at index=0 and entrynumber at index=1
-            //from the current index of rawListEntries. It will be then put into listNumbers
-            //and entryNumbers
-
-            tmpSplitter = rawListEntries[i].split("/");
-            listNumbers[i]  = Integer.valueOf(tmpSplitter[0]);
-            entryNumbers[i] = Integer.valueOf(tmpSplitter[1]);
-        }
-
-        if (tmpSplitter != null) {
-            Log.d("SharedPrefs:    ", "N:   "+ Arrays.toString(listNumbers));
-            Log.d("SharedPrefs:    ", "E:   "+ Arrays.toString(entryNumbers));
-        }
-
-        //Returning two arrays at the same time. Alternative solution? I don't like this one -_-
-        return new int[][] {listNumbers, entryNumbers};
-    }
-
-    static void saveFragen(String fragen) {
-        editor = prefs.edit();
-        editor.putString("Fragen", fragen);
-        editor.apply();
-    }
-    static String[] getFragen() {
-        String [] fragen = null;
-        return fragen;
+    static int getCurrentQuestionId() {
+        return prefs.getInt(INDEX_NUMBER, 1);
     }
 }
