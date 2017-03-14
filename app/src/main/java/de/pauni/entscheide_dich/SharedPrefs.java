8b0f3a7621532060867825c5f87c8fe8be94e6b8
Roni <roni-96@live.de>
2017-03-14 17:21:08 +0100

    only init db if 1. start

--- app/src/main/java/de/pauni/entscheide_dich/SharedPrefs.java
+++ app/src/main/java/de/pauni/entscheide_dich/SharedPrefs.java
@@ -20,23 +20,31 @@ class SharedPrefs extends Activity {
     private static SharedPreferences.Editor editor;
 
     //the keys of the things to save
-    private static final String INDEX_NUMBER = "question_index";
+    private static final String CURRENT_QUESTION_ID = "question_index";
     private static final String FIRST_START  = "first_start";
 
     SharedPrefs(Context c) {
-        //Maybe I will need the context variable later...
         prefs = c.getSharedPreferences(PREFS, 0);
     }
 
-
-
-
     static void saveQuestionId(int value) {
         editor = prefs.edit();
-        editor.putInt(INDEX_NUMBER, value);
+        editor.putInt(CURRENT_QUESTION_ID, value);
         editor.apply();
     }
+
+    static boolean isFirstStart() {
+        //If this FIRST_START doesn't exist, return true and change it to false.
+        //Else return false
+        if( prefs.getBoolean(FIRST_START, true)) {
+            editor = prefs.edit();
+            editor.putBoolean(FIRST_START, false);
+            editor.apply();
+            return true;
+        } else return false;
+    }
+
     static int getCurrentQuestionId() {
-        return prefs.getInt(INDEX_NUMBER, 1);
+        return prefs.getInt(CURRENT_QUESTION_ID, 1);
     }
 }
